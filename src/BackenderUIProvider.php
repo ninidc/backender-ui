5333a175bb12999842ad91becfe81395043f19d9
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-05-15 18:55:33 +0200

    Configure provider

---
+++ src/BackenderUIProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Backender\UI;
+
+use Config;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class BackenderUIProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        $this->loadViewsFrom(__DIR__.'/Resources/views', 'backender:ui');
+    }   
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
